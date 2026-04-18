5297d18aa85fd40a221509f2dfac0c8b87073458
unknown <malkocmax@gmail.com>
2026-04-18 20:22:11 +0300

    feat: remove rarity filter from knife and glove pages

--- app/pages/glove/[slug].vue
+++ app/pages/glove/[slug].vue
@@ -45,28 +45,6 @@
             </svg>
             <input v-model="searchQuery" placeholder="Поиск по названию" class="search-box__input" />
           </div>
-
-          <div class="filter-dropdown">
-            <button @click="showRarityFilter = !showRarityFilter" class="filter-btn">
-              <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M3 4h18M3 12h18M3 20h18"/></svg>
-              <span>{{ selectedRarity || 'Все редкости' }}</span>
-            </button>
-            <Transition name="dropdown">
-              <div v-if="showRarityFilter" class="filter-dropdown__menu">
-                <button @click="selectRarity(null)" class="filter-dropdown__item" :class="{ 'filter-dropdown__item--active': !selectedRarity }">Все редкости</button>
-                <button
-                  v-for="rarity in rarities" :key="rarity.name"
-                  @click="selectRarity(rarity.name)"
-                  class="filter-dropdown__item"
-                  :class="{ 'filter-dropdown__item--active': selectedRarity === rarity.name }"
-                  :style="{ '--rarity-color': rarity.color }"
-                >
-                  <span class="filter-dropdown__dot"></span>
-                  {{ rarity.name }}
-                </button>
-              </div>
-            </Transition>
-          </div>
         </div>
       </div>
 
