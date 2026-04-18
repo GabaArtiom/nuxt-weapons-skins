a2af8b028a527b2aa5724468e22d3f20f354d04b
unknown <malkocmax@gmail.com>
2026-04-18 20:04:13 +0300

    feat: add team icons to toggle component
    - Create IconCT component with SVG logo
    - Create IconT component with webp image
    - Update TeamToggle to use icon components instead of text
    - Add styles for icons in team toggle badges
    - Add Icon-t-patch-small.webp asset

---
+++ app/components/IconT.vue
@@ -0,0 +1,3 @@
+<template>
+  <img src="~/assets/img/Icon-t-patch-small.webp" alt="T" />
+</template>
--- app/components/TeamToggle.vue
+++ app/components/TeamToggle.vue
@@ -3,10 +3,14 @@
     <div class="team-toggle__track">
       <div class="team-toggle__slider" :class="{ 'team-toggle__slider--t': modelValue === 2 }"></div>
       <div class="team-toggle__option team-toggle__option--ct" :class="{ 'team-toggle__option--active': modelValue === 3 }">
-        <span class="team-toggle__badge team-toggle__badge--ct">CT</span>
+        <span class="team-toggle__badge team-toggle__badge--ct">
+          <IconCT />
+        </span>
       </div>
       <div class="team-toggle__option team-toggle__option--t" :class="{ 'team-toggle__option--active': modelValue === 2 }">
-        <span class="team-toggle__badge team-toggle__badge--t">T</span>
+        <span class="team-toggle__badge team-toggle__badge--t">
+          <IconT />
+        </span>
       </div>
     </div>
   </button>
